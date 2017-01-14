125b8160f33a81610a41b2b9d892702edc21a8c0
Kieron Gillespie <kieron.gillespie@anaren.com>
2017-01-14 10:05:04 -0500

    Dropping some of the output.

--- build_arduino101.sh
+++ build_arduino101.sh
@@ -4,6 +4,8 @@ PATH=$PATH:./tools-builder/ctags/5.8-arduino11/:/opt/arduino101-sdk/packages/Int
 
 ./arduino-builder -build-path $1 -hardware ./hardware -hardware ./packages/Intel/hardware -tools ./packages -tools ./tools-builder -libraries ./packages/Intel/hardware/intel/arc32/libraries -fqbn intel:arc32:arduino_101 $2
 
+exec > /dev/null 2>&1
+
 pushd $1
 pushd ../
 arc-elf32-objcopy -O binary arduino101_sketch.debug.elf firmware.bin
